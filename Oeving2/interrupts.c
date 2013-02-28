26b0d65ac62533a377ae3be80280a806879df11c
x10an14 <x10an14@hotmail.com>
2013-02-28 11:57:00 +0100

    Added interrupts.c file

---
+++ Oeving2/interrupts.c
@@ -0,0 +1,22 @@
+#include "oeving2.h"
+
+typedef void (*__int_handler)(void) {
+
+}
+
+__int_handler register_interrupt(__int_handler handler, int group,
+    int line, int priority){
+
+}
+
+void set_interrupts_base(void *addr){
+
+}
+
+void init_interrupts(void){
+
+}
+
+void panic(void){
+
+}
