41386ce6d9f979cdcfcc86f6aaf69e2ff655cb04
x10an14 <x10an14@hotmail.com>
2013-03-03 20:13:02 +0100

    Smaller additions...

--- Oeving2/oeving2.c
+++ Oeving2/oeving2.c
@@ -12,7 +12,7 @@ volatile avr32_pm_t *pm = &AVR32_PM;
 volatile avr32_abdac_t *abdac = &AVR32_ABDAC;
 
 
-int main (int argc, char *argv[]) {
+int main (int argc, char *argv[]){
   initHardware();
 
   while(1);
@@ -20,7 +20,7 @@ int main (int argc, char *argv[]) {
 }
 
 /* funksjon for å initialisere maskinvaren, må utvides */
-void initHardware (void) {
+void initHardware (void){
   initIntc();
   initLeds();
   initButtons();
@@ -28,36 +28,41 @@ void initHardware (void) {
 }
 
 /* Flytter denne metoden ut i en egen C fil. Legger og til tilhørende metoder der.
-void initIntc(void) {
+void initIntc(void{
   set_interrupts_base((void *)AVR32_INTC_ADDRESS);
   init_interrupts();
 }*/
 
-void initButtons(void) {
+void initButtons(void){
   //No clue so far what this line does
-  register_interrupt( button_isr, AVR32_PIOB_IRQ/32, AVR32_PIOB_IRQ % 32, BUTTONS_INT_LEVEL);
+  register_interrupt(button_isr, AVR32_PIOB_IRQ/32, AVR32_PIOB_IRQ % 32, BUTTONS_INT_LEVEL);
   piob->per = 0xe0; //Switches 7-5 active
   piob->puer = 0xe0;
   piob->ier = 0xe0;
   //Disable the rest of the switches
   piob->idr = 0x1f;
+  button_isr();
 }
 
-void initLeds(void) {
+void initLeds(void){
   //Enable all LEDs on PIOB
   pioc->per = 0xfF; //0xff == all LEDs
   pioc->oer = 0xff;
 }
 
-void initAudio(void) {
-  register_interrupt( abdac_isr, AVR32_ABDAC_IRQ/32, AVR32_ABDAC_IRQ % 32, ABDAC_INT_LEVEL);
-  /* (...) */
+void initAudio(void){
+  register_interrupt(abdac_isr, AVR32_ABDAC_IRQ/32, AVR32_ABDAC_IRQ % 32, ABDAC_INT_LEVEL);
+  piob->PDR.p20 = 1;
+  piob->PDR.p21 = 1;
+  piob->ASR.p20 = 1;
+  piob->ASR.p21 = 1;
+  abdac_isr();
 }
 
-void button_isr(void) {
+void button_isr(void){
 
 }
 
-void abdac_isr(void) {
+void abdac_isr(void){
 
 }
