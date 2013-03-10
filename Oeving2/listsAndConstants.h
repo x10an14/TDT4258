f4a4521a5a1974a8f279ba6e2fd41da5d6226954
x10an14 <x10an14@hotmail.com>
2013-03-10 19:38:00 +0100

    Test

--- Oeving2/listsAndConstants.h
+++ Oeving2/listsAndConstants.h
@@ -17,9 +17,9 @@
 #define SW3 0x8
 
 /*Obsolete? Rates of which we will play each single element before moving on to next element (creating pitch)*/
-//#define SAWRATE 20
-//#define SQUARERATE 11
-//#define TRIANGLERATE 18
+#define SAWRATE 20
+#define SQUARERATE 11
+#define TRIANGLERATE 18
 
 
 //#define TONE#OCTAVE AmountOfRepetitionsPerSquareWaveElementPerSecondToGenerateSaidTone
