f8f0427ee9da62a3eb7451abbbedf66705dd60e4
x10an14 <x10an14@hotmail.com>
2013-03-13 20:24:15 +0100

    Committing proper code

--- Oeving2/oeving2_InterruptFunctions.c
+++ Oeving2/oeving2_InterruptFunctions.c
@@ -6,7 +6,7 @@ short volatile newButtonState;
 
 void button_isr(void){
 	newButtonState = ~piob->pdsr;//Read which switch was pushed (invert)
-	newButtonState &= piob->isr;//Read interrupt vector, and with pushed button-value, and enabling next interrupt
+	newButtonState &= piob->isr;//Read interrupt vector, and with pushed button-value, and enabling next interrupt (Kind of debouncing too...)
 	if(newButtonState > 0){
 		pioc->codr = 0xff;//Turn off all the lights
 	}
@@ -21,15 +21,15 @@ void button_isr(void){
 	} else if(newButtonState == SW5){//Switch05
 		setAbdacOnOff(1);
 		currentSample = squareSample;
-	} else if(newButtonState == SW4){//Switch04
-		setAbdacOnOff(1);
-		currentSample = flaaklypaSample;
-	// } else if(newButtonState == SW3){//Switch03 /*Below code is commented out because: NOT WORKING!*/
+	// } else if(newButtonState == SW4){//Switch04 /*Below code is commented out because: NOT WORKING!*/
+	// 	setAbdacOnOff(1);
+	// 	currentSample = flaaklypaSample;
+	// } else if(newButtonState == SW3){//Switch03
 	// 	setAbdacOnOff(1);
 	// 	currentSample = scaleSample;
-	// } else if(newButtonState == SW2){//Switch02
-	// 	setAbdacOnOff(1);
-	// 	currentSample = sineSample;
+	} else if(newButtonState == SW2){//Switch02
+		setAbdacOnOff(1);
+		currentSample = sineSample;
 	// }/* else if(newButtonState == SW1){//Switch01*/
 	} else if(newButtonState == SW0){//Switch0
 		/*SILENCE WILL FALL...*/
