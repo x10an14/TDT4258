ceb33813a5ab4ae2e636cfe1b40c8f65bd96e5fa
x10an14 <x10an14@hotmail.com>
2013-03-13 23:09:28 +0100

    Fixed comments in oeving2.h

--- Oeving2/oeving2.h
+++ Oeving2/oeving2.h
@@ -11,29 +11,34 @@
 
 // #include <avr32/ap7000.h> /* inkludere Atmel-headerfil */
 // #include <sys/interrupts.h>
-//Commented out above so that we can compile on our own machines (see lines below)
+//Commented out the above two includes so that we can compile on our own machines (Added needed includes below)
 
 #include "sys/interrupts.h"
 #include "avr32/ap7000.h"
 #include "avr32/abdac_101.h"
+//The below include is for a header file we've written ourselves.
 #include "listsAndConstants.h"
 
 
-/*Structs we've made ourselves for this assignment*/
+//Below are two structs we've made ourselves for this assignment
 
+/*A "sample" struct holding both a list of amplitue or tone values, and potentially a list of strokes.
+It also contains the size of the two lists, and an int member used as a bool (usingStrokeList), as well
+as two counters and another maxvalue-member (similar to size)*/
 typedef struct sample{
-		//List of short-values (amplitude/tone values) for tune that this struct holds
+		//List of short-values (amplitude or tone values) for the tune that this struct holds
 		short *list;
-		//A list of short values carrying the X in Y/X where Y always equals 1. (To get tones of different time length)
+		//A list of short values carrying the X in 1/X-fraction that will always be how much of a second the corresponding tone in list should be played.
 		short *strokeList;
-		//Int used as bool for whether list is used with strokeList or rateCntr
+		//Int used as bool for whether list is played with the help of strokeList or rateCntr
 		short usingStrokeList;
-		//Counter and size for both abovementioned lists (Int's because they're so fricking huge numbers)
+		//Counter and size for both abovementioned lists (int's because they're so fricking huge numbers)
 		int playCntr, size;
-		//Counter and maxvalue for rate of playing (If timelist is not used, these two are meant to be used instead)
+		//Counter and maxvalue for rate of playing each element in list. Used when strokeList is not used. These two members will also affect pitch however, which the two previous will not if used as intended.
 		short rateCntr, rateMax;
 }sample;
 
+/*A "sampleCollection" struct which is meant to have an iterable collection of samples. (Used in main (outcommented code - for now))*/
 typedef struct sampleCollection{
 		//List to pointers which point a sample struct each
 		sample **list;
@@ -41,12 +46,19 @@ typedef struct sampleCollection{
 		short size;
 }sampleCollection;
 
+/*Pointers which are should be possible to use "globally" between the C-files.
+The pointers are used for interacting with the STK1000.*/
+
+//Buttons ParallellInputOutput pointer
 extern volatile avr32_pio_t *piob;
+//LEDs ParallellInputOutput pointer
 extern volatile avr32_pio_t *pioc;
+//Pointer to PowerManager
 extern volatile avr32_pm_t *pm;
+//Pointer to ABDAC controller
 extern volatile avr32_abdac_t *abdac;
 
-//All below are global
+//All below lists are used solely in main, and therefore do not need to have the type modifier extern.
 sample *flaaklypaSample, *flaa1, *flaa2, *flaa3, *flaa4, *squareSample,
 		*sawSample, *triangleSample, *scaleSample, *sineSample, *currentSample;
 sampleCollection *flaaklyp;
@@ -61,6 +73,8 @@ void initHardware (void);
 
 void button_isr(void);
 void abdac_isr(void);
+
+//Our own added prototypes
 sample* addFrequency(sample *inpt, short stroke, short tone, int start);
 int getAmountOfPeriods(int stroke, short tone);
 void addZeroes(int amount, short *list, int start);
