8b124e7882e11247e05222c1366808d3a8cae8f7
x10an14 <x10an14@hotmail.com>
2013-03-13 23:19:33 +0100

    Fixed comments in listsAndConstants.h

--- Oeving2/listsAndConstants.h
+++ Oeving2/listsAndConstants.h
@@ -1,10 +1,8 @@
 /* legg andre "includes" her, sammen med "defines" */
 //Moved the above line to this file instead!
 
-#ifndef OEVING_2_LISTS_AND_CONSTANTS
-#define OEVING_2_LISTS_AND_CONSTANTS
-
-#include "limits.h"
+#ifndef OEVING_2_H_LISTS_AND_CONSTANTS
+#define OEVING_2_H_LISTS_AND_CONSTANTS
 
 /* macroes!!!! \o/ */
 #define ABDAC_INT_LEVEL 0
@@ -14,7 +12,7 @@
 #define ARRAYSIZE 16
 #define SINEARRAYSIZE 128
 
-/*Button values in hex*/
+/*Button values in hex values*/
 #define SW7 0x80
 #define SW6 0x40
 #define SW5 0x20
@@ -24,13 +22,13 @@
 #define SW1 0x2
 #define SW0 0x1
 
-/*Obsolete? Rates of which we will play each single element before moving on to next element (creating pitch)*/
+/*Rates of which we will play each single element before moving on to next element (manipulating pitch and time each element will be played simultaneously)*/
 #define SAWRATE 6
 #define TRIANGLERATE 8
 #define SQUARERATE 16
 #define SINERATE 1
 
-//#define TONE#OCTAVE AmountOfRepetitionsPerSquareWaveElementPerSecondToGenerateSaidTone
+//List of macroes, where we have the name of a tone, which will be replaced with the frequency of said tone divided by 2. This is because we're presupposing we will use a square waveform to play the listts using these tones.
 #define G_3 104
 #define A3 110
 #define B3 123
@@ -43,30 +41,33 @@
 #define B4 247
 #define C5 262
 
+//The rate at which the ABDAC samples each output ((12*1000^2)/256)
 #define ABDAC_SAMPLERATE 46875
 
-/* megalists! \o/ */
+//Lists which in theory can be used in every single C-file, so therefore they are declared with type-modifier extern. Have not tested whether it is needed, because we only really use them in oeving2_main.c
+//Tone-lists which together in specific order compose the tune "Reodors Ballade" from the famous norwegian story "Flåklypa"
 extern short FLAA1[];
 extern short FLAA2[];
 extern short FLAA3[];
 extern short FLAA4[];
 
+//Stroke-lists for each of the above tone-lists.
 extern short FLAASTROKE1[];
 extern short FLAASTROKE2[];
 extern short FLAASTROKE3[];
 extern short FLAASTROKE4[];
 
-// Scale roll 8 steps
+//A tone-list and corresponding stroke-list for the scaleSample in main.
 extern short SCALE[];
 extern short SCALESTROKE[];
 
-// Saw Waveform 17 steps
+//Declaration of the saw waveform list
 extern short SAW[];
 
-// Triangle Waveform 17 steps
+//Declaration of the triangle waveform list
 extern short TRIANGLE[];
 
-// Square Waveform 17 steps
+//Declaration of the square waveform list
 extern short SQUARE[];
 
 #endif
