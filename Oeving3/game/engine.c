78785de1624f5d42c3efe3fb03c120d4d2b0d67c
x10an14 <x10an14@hotmail.com>
2013-04-22 18:44:35 +0200

    Added prototypes and game description

---
+++ Oeving3/game/engine.c
@@ -0,0 +1,4 @@
+
+/* Prototypes */
+void movePlayer(/*Modifiable object for directions? or simply an int/char*/);
+void playSound(/*Pointer to sound to play (we were told that we were given some in the handed out code, no?*/);
--- Oeving3/game/main.c
+++ Oeving3/game/main.c
@@ -4,12 +4,16 @@
 /* Project include files */
 #include "include/listsAndConstants.h"
 
-/*This game idea (not following the description in the compendium), is a game where the goal is pure and simple survival.
+/* -----------!Game description!-----------
+
+This game idea (not following the description in the compendium), is a game where the goal is pure and simple survival.
 There will hopefully (if there's time) be animals which you should avoid to survive, using the SWITCH buttons on the board to move around on the map displayed on the display.
 The map will (again, hopefully if we finish on time), consist of rivers, bushes, trees and rocks.
 The goal of the game is to move as far as possible (distance, not amount of steps) away from the starting point.
 Ideally, the map should automagically generate "new" areas as you move around, but let's see how far we get with that.
 The LEDs on the board will be indicators of your current health/hunger.*/
+
+
 
 int main(){
 	printf("Hahahaha test!\n");
