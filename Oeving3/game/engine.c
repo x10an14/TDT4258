4e8566dba72a6d916a9dbc7dab75d505ba5cd620
x10an14 <x10an14@hotmail.com>
2013-04-25 19:31:06 +0200

    Added ability of reallocing Objects list into Object (by having max sizes on list)

--- Oeving3/game/engine.c
+++ Oeving3/game/engine.c
@@ -26,12 +26,47 @@ void loseHealth(Type type, int listIndex, int amount){
 	}
 }
 
+int isPlayerInsideScreen(int, listIndex, int nextX){
+	Form *player = container->playerList[listIndex]->form;
+	if(nextX >= 0 && nextX < SCREEN_WIDTH){
+		return 1;
+	}
+	return 0;
+}
+
+int isEnemyInsideScreen(int listIndex, int nextX, int nextY){
+	Form *enemy = container->enemyList[listIndex]->form;
+	if(nextX >= 0 && nextX < SCREEN_WIDTH &&
+		nextY >= 0 && nextY < SCREEN_HEIGH){
+		return 1;
+	}
+	return 0;
+}
+
+int isShotInsideScreen(int listIndex){
+
+}
+
 void computeMove(Type type, int listIndex){
 	switch(type){
 		case PLAYER:
+		{Form *playForm = container->playerList[listIndex]->form;
+		int nextX = playForm->x + playForm->dx + playForm->radius;
+		if(isPlayerInsideScreen(listIndex, nextX)){
+			playForm->x = nextX - playForm->radius;
+		}
+		redraw_square(playForm);}
 		break;
 
 		case ENEMY:
+		{Form *enemForm = container->enemyList[listIndex]->form;
+		int nextX = enemForm->x + enemForm->dx + enemForm->radius;
+		int nextY = enemForm->y + enemForm->dy + enemForm->radius;
+		if(isEnemyInsideScreen(listIndex, nextX, nextY)){
+			enemForm->x = nextX - enemForm->radius;
+			enemForm->y = nextY - enemForm->radius;
+		}
+		redraw_square(enemForm);}
 		break;
 
 		case SHOT:
@@ -42,7 +77,6 @@ void computeMove(Type type, int listIndex){
 void startGame(){
 	/* Infinite loop for now */
 	int i;
-	draw(container->playerList[0]->form);
 	while(1){
 		usleep(30000);
 		make_new_frame(container);
