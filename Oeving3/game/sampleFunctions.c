a8bc8c116656d0ad2d0bba40c1fa3a1d698d35c8
x10an14 <x10an14@hotmail.com>
2013-04-25 19:30:15 +0200

    Written first implementation of insertFirstEnemy

--- Oeving3/game/sampleFunctions.c
+++ Oeving3/game/sampleFunctions.c
@@ -6,20 +6,54 @@
 
 
 void insertPlayers(Objects *container, int amountOfPlayers){
-	Player **List = malloc(sizeof(Player*) * amountOfPlayers);
+	Player **List = malloc(sizeof(Player*)*2);
 	int i;
 	for(i = 0; i < amountOfPlayers; i++){
 		List[i] = malloc(sizeof(Player));
+		List[i]->health = 250; List[i]->healthMax = 250;
 		List[i]->form = (Form*) malloc(sizeof(Form));
-		List[i]->form->x = 150; List[i]->form->y = 150;
+		int radius = 15;
+		List[i]->form->radius = radius;
+		List[i]->form->x = 150; List[i]->form->y = SCREEN_HEIGHT-(radius/2);
 		List[i]->form->dx = 0; List[i]->form->dy = -5;
-		List[i]->form->radius = 15;
 		List[i]->form->formType = SQUARE;
-		List[i]->form->red = 0; List[i]->form->blue = 0;
+		List[i]->form->red = 0; List[i]->form->blue = 255;
 		List[i]->form->green = 0;
+		draw(List[i]->form);
 	}
 	container->playerList = List;
 	container->playerSize = amountOfPlayers;
+	container->playerMax = 2;
 }
 
+void insertShot(Objects *container, int startX, int startY, int damage){
 
+}
+
+void insertFirstEnemy(Objects *container){
+	Enemy **List = malloc(sizeof(Enemy*));
+	List[i] = malloc(sizeof(Enemy));
+
+	List[i]->health = 75; List[i]->healthMax = 75;
+	Form *form = malloc(sizeof(Form));
+	int radius = 10;
+	form->radius = radius;
+	form->x = radius + 10;
+	form->y = radius + 10;
+	form->formType = SQUARE;
+	form->red = 255; form->blue = 0;
+	form->green = 0;
+	List[i]->form = form;
+	draw(List[i]->form);
+
+	container->enemyList = List;
+	container->enemyMax = 1;
+	container->enemySize = 1;
+}
+
+void insertOneEnemy(Objects *container, int startX, int startY, int health){
+	if(container->enemySize == container->enemyMax){
+		container->enemyList = realloc(container->enemyList, 2*container->enemyMax*sizeof(Enemy*));
+	}
+
+}
