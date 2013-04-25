ae951eaf5a07759a66f03fcedb093243dae3a05e
x10an14 <x10an14@hotmail.com>
2013-04-25 20:00:20 +0200

    Testing

--- Oeving3/game/sampleFunctions.c
+++ Oeving3/game/sampleFunctions.c
@@ -17,7 +17,7 @@ void insertPlayers(Objects *container, int amountOfPlayers){
 		List[i]->form = (Form*) malloc(sizeof(Form));
 		int radius = 15;
 		List[i]->form->radius = radius;
-		List[i]->form->x = 150; List[i]->form->y = SCREEN_HEIGHT-(radius/2);
+		List[i]->form->x = 150; List[i]->form->y = SCREEN_HEIGHT-(radius/2)-4;
 		List[i]->form->dx = 0; List[i]->form->dy = -5;
 		List[i]->form->formType = SQUARE;
 		List[i]->form->red = 0; List[i]->form->blue = 255;
