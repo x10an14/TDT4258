0ec85a156b2a512eab2c7eec4fb8c71b31f9237c
x10an14 <x10an14@hotmail.com>
2013-04-25 15:04:20 +0200

    Fixed Graphics.c

--- Oeving3/game/graphics.c
+++ Oeving3/game/graphics.c
@@ -1,21 +1,30 @@
 //Whatever includes we need to utilize our screen...
 #include <stdio.h>
 #include <errno.h>
+
+/* Project includes */
 #include "include/listsAndConstants.h"
 #include "include/sampleStructs.h"
 
 char background_red = 15;
 char background_green = 225;
 char background_blue = 15;
-FILE *lcdDriver;
+FILE *screen;
 
-void setImageStruct(void){
-	lcdDriver = (FILE*) fopen("/dev/fb0","r+");
-	printf("Opened lcdDriver file: %d errno %s\n", lcdDriver, strerror(errno));
-	draw_background(lcdDriver);
+/* Prototypes */
+void setImageStruct(void);
+int draw_background(void);
+int draw_square(int x, int y, int radius, char red, char green, char blue);
+int redraw_player(player *thePlayer);
+int make_new_frame(player *thePlayer);
+
+void setImageStruct(){
+	screen = (FILE*) fopen("/dev/fb0","r+");
+	printf("Opened screen file: %d errno %s\n", screen, strerror(errno));
+	draw_background(screen);
 }
 
-int draw_background(FILE* screen){
+int draw_background(){
 	int addr = 0;
 	fseek(screen, 0, 0);
 	while(addr < SCREEN_WIDTH*SCREEN_HEIGHT*3){
@@ -27,7 +36,7 @@ int draw_background(FILE* screen){
 	return 1;
 }
 
-int draw_square(FILE* screen, int x, int y, int radius, char red, char green, char blue){
+int draw_square(int x, int y, int radius, char red, char green, char blue){
 	int file_Y, file_X;
 	for (file_Y = (y-radius)*SCREEN_WIDTH*3 ; file_Y < (y+radius)*SCREEN_WIDTH*3 ; file_Y+=SCREEN_WIDTH*3){
 		file_X = (x-radius)*3;
@@ -41,14 +50,14 @@ int draw_square(FILE* screen, int x, int y, int radius, char red, char green, ch
 	}
 }
 
-int redraw_player(FILE* screen, player* thePlayer){
-	draw_square(screen, thePlayer->playerX, thePlayer->playerY, thePlayer->radius, background_red, background_green, background_blue);
+int redraw_player(player* thePlayer){
+	draw_square(thePlayer->playerX, thePlayer->playerY, thePlayer->radius, background_red, background_green, background_blue);
 	thePlayer->playerX += thePlayer->dx;
 	if ((thePlayer->playerX + thePlayer->dx > SCREEN_WIDTH - thePlayer->radius) || (thePlayer->playerX + thePlayer->dx < 0+thePlayer->radius)){
 		thePlayer->dx = -thePlayer->dx;
 	} else
 		thePlayer->playerY += thePlayer->dy;
-	draw_square(screen, thePlayer->playerX, thePlayer->playerY, thePlayer->radius, thePlayer->col_red, thePlayer->col_green, thePlayer->col_blue);
+	draw_square(thePlayer->playerX, thePlayer->playerY, thePlayer->radius, thePlayer->col_red, thePlayer->col_green, thePlayer->col_blue);
 }
 
 int make_new_frame(player* thePlayer){
@@ -64,12 +73,9 @@ int make_new_frame(player* thePlayer){
 	//	background_red+=dRed;
 	//	background_green+=dGreen;
 	//}
-	redraw_player(lcdDriver, thePlayer);
+	redraw_player(screen, thePlayer);
 
 	//draw_background(screen, background_red, background_green, background_blue);
 	return 0;
 }
 
-void generateMap(FILE *lcdDriver){
-
-}
