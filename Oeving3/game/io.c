05232a35622b6a255bbe3209effc4b3c164966a6
Fedor Olegovich Fadeev <fadeev@dmlab04.idi.ntnu.no>
2013-04-25 19:33:06 +0200

    buttons-functions implemented

--- Oeving3/game/io.c
+++ Oeving3/game/io.c
@@ -1,5 +1,7 @@
 //Whatever includes we need to utilize our drivers.
-
+#include <stdio.h>
+#include <errno.h>
+FILE* buttonsDriver;
 
 int A(void){
 	return 0;
@@ -11,4 +13,29 @@ int B(void){
 
 int C(void){
 	return 0;
+}
+
+int initiateButtons(){
+	buttonsDriver = (FILE*) fopen("/dev/swdriver","r+");
+	printf("Opened buttons-driver file: %d errno %s?22\n", buttonsDriver, strerror(errno));
+}
+
+
+char getButtonsState(){
+	char* buff[3];
+	fgets(buff, 3, buttonsDriver);
+	int buttonStatus;
+	sscanf(buff, "%x\n", &buttonStatus);
+	return buttonStatus;
+}
+int isButtonDown(int buttonNumber){
+	char* buff[3];
+	fgets(buff, 3, buttonsDriver);
+	int buttonStatus;
+	sscanf(buff, "%x\n", &buttonStatus);
+	buttonStatus = (buttonStatus >> buttonNumber) & 1;
+	if (buttonStatus)
+		return 0;
+	else
+		return 1;
 }
---
+++ Oeving3/swdriver.c
@@ -0,0 +1,126 @@
+/*****************************************************************************
+ *
+ * Øving 3 uCSysDes, driverkoden
+ *
+ *****************************************************************************/
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/ioport.h>
+#include <asm/io.h>
+#include <asm/uaccess.h>
+
+#include "ap7000.h"
+
+/* prototyper */
+
+static int __init driver_init(void);
+static void __exit driver_exit(void);
+static int driver_open (struct inode *inode, struct file *filp);
+static int driver_release (struct inode *inode, struct file *filp);
+static ssize_t driver_read (struct file *filp, char __user *buff,
+                     size_t count, loff_t *offp);
+static ssize_t driver_write (struct file *filp, const char __user *buff,
+                      size_t count, loff_t *offp);
+
+/* fops-struct */
+
+static struct file_operations driver_fops = {
+  .owner = THIS_MODULE,
+  .read = driver_read,
+  .write = driver_write,
+  .open = driver_open,
+  .release = driver_release
+};
+
+/* major & minor numre */
+
+
+/*golbale variabler : */
+
+volatile avr32_pio_t *piob = &AVR32_PIOB;  // LED
+
+/*****************************************************************************/
+/* init-funksjon (kalles når modul lastes) */
+
+int say_hello(void){
+    printk(KERN_ALERT "SW driver initiated!\n");
+  return 0;
+}
+
+int setup_SW(void){
+  piob->per = 0xff;
+  piob->puer = 0xff;
+  return 0;
+}
+int read_SW(void){
+  return piob->pdsr % 0x100;
+}
+
+static int __init driver_init (void) {
+  int reg_succeded;
+  setup_SW();
+  say_hello();
+  reg_succeded = register_chrdev(0, "swdriver", &driver_fops);
+  printk(KERN_ALERT "reg success: %d\n", reg_succeded);
+
+  int readSW = read_SW();
+  printk(KERN_ALERT "read buttons are: %x +10 = %x\n", readSW, readSW+10);
+  return 0;
+}
+
+/*****************************************************************************/
+/* exit-funksjon (kalles når modul fjernes fra systemet) */
+
+static void __exit driver_exit (void) {
+  printk(KERN_ALERT "goodbye world\n");
+}
+
+/*****************************************************************************/
+/* fops-funksjoner */
+
+static int driver_open (struct inode *inode, struct file *filp) {
+  return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+
+static int driver_release (struct inode *inode, struct file *filp) {
+  return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+
+static ssize_t driver_read (struct file *filp, char __user *buff,
+              size_t count, loff_t *offp) {
+  size_t out;
+  sprintf(buff, "%x", read_SW());
+  out = count;
+
+  //printk(KERN_ALERT "read called, ouputting %x, out = %d\n", currentLedStatus, out);
+  return out;
+}
+
+/*---------------------------------------------------------------------------*/
+
+static ssize_t driver_write (struct file *filp, const char __user *buff,
+               size_t count, loff_t *offp) {
+  printk(KERN_ALERT "NO WRITE ALLOWED\n");
+  return -1;
+}
+
+/*****************************************************************************/
+/* modul-beskrivelsesmakroer */
+
+module_init (driver_init);  /* angir hva som er init-funksjon */
+module_exit (driver_exit);  /* angir hva som er exit-funksjon */
+
+MODULE_LICENSE ("GPL");     /* programlisens for modulen */
+MODULE_DESCRIPTION ("");    /* tekstlig beskrivelse */
+MODULE_VERSION ("");        /* versjonsnummer */
+MODULE_AUTHOR ("");         /* forfatter(e) */
+
